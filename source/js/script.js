4be6c4fdd8251276e6236f1e50a8b32c0ba197d7
alekseyburn <alekseyburn@gmail.com>
2019-06-21 21:18:57 +0300

    стилизовал index mobile

--- source/js/script.js
+++ source/js/script.js
@@ -21,3 +21,28 @@ let menuToggle = function() {
 };
 
 menuToggle();
+
+let catalogToggle = function () {
+  let catalog = document.querySelectorAll(".catalog__info-wrapper");
+  let catalogToggler = document.querySelectorAll(".catalog__btn");
+
+  if (!catalog || !catalogToggler) return;
+
+  // Array.prototype.forEach.call(catalog, function(catalog) {
+  //   Array.prototype.forEach.call(catalogToggler, function (catalogToggler) {
+  //     catalogToggler.addEventListener("click", function (event) {
+  //       event.preventDefault();
+  //       catalog.classList.toggle("catalog__info-wrapper--open");
+  //     });
+  //   });
+  // });
+
+  for (let i = 0; i < catalog.length; i++) {
+    catalogToggler[i].addEventListener("click", function (event) {
+      event.preventDefault();
+      catalog[i].classList.toggle("catalog__info-wrapper--open");
+    });
+  }
+};
+
+catalogToggle();
